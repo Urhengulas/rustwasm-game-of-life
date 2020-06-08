99dfcdbb76b249e0815e30ef37d80693c19d4f23
Urhengulas <johann.hemmann@code.berlin>
2020-06-08 14:50:05 +0200

    Add constructor and renderer to Universe

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use std::fmt;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -46,6 +47,27 @@ impl Universe {
 
 #[wasm_bindgen]
 impl Universe {
+	pub fn new() -> Universe {
+		let width = 64;
+		let height = 64;
+
+		let cells = (0..(width * height))
+			.map(|i| {
+				if i % 2 == 0 || i % 7 == 0 {
+					Cell::Alive
+				} else {
+					Cell::Dead
+				}
+			})
+			.collect();
+
+		Universe {
+			width,
+			height,
+			cells,
+		}
+	}
+
 	pub fn tick(&mut self) {
 		let mut next = self.cells.clone();
 
@@ -73,5 +95,22 @@ impl Universe {
 		}
 
 		self.cells = next;
+	}
+
+	pub fn render(&self) -> String {
+		self.to_string()
+	}
+}
+
+impl fmt::Display for Universe {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		for line in self.cells.as_slice().chunks(self.width as usize) {
+			for &cell in line {
+				let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+				write!(f, "{}", symbol)?;
+			}
+			write!(f, "\n")?;
+		}
+		Ok(())
 	}
 }
