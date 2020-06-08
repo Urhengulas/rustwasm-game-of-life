e808129dcffad8af8e860a62ce8ab2b8294305d2
Urhengulas <johann.hemmann@code.berlin>
2020-06-08 14:34:34 +0200

    Implement universe

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,69 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+	Dead = 0,
+	Alive = 1,
 }
 
 #[wasm_bindgen]
-pub fn greet(msg: &str) {
-    alert(&*format!("Hello, {}", msg));
+pub struct Universe {
+	width: u32,
+	height: u32,
+	cells: Vec<Cell>,
+}
+impl Universe {
+	fn get_index(&self, row: u32, column: u32) -> usize {
+		(row * self.width + column) as usize
+	}
+	fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+		let mut count = 0;
+		for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+			for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+				if delta_row == 0 && delta_col == 0 {
+					continue;
+				}
+
+				let neighbor_row = (row + delta_row) % self.height;
+				let neighbor_col = (column + delta_col) % self.width;
+				let idx = self.get_index(neighbor_row, neighbor_col);
+				count += self.cells[idx] as u8;
+			}
+		}
+		count
+	}
+}
+
+#[wasm_bindgen]
+impl Universe {
+	pub fn tick(&mut self) {
+		let mut next = self.cells.clone();
+
+		for row in 0..self.height {
+			for col in 0..self.width {
+				let idx = self.get_index(row, col);
+				let cell = self.cells[idx];
+				let live_neighbors = self.live_neighbor_count(row, col);
+
+				let next_cell = match (cell, live_neighbors) {
+					// Rule 1: Cell::Alive AND (live_neighbors < 2) -> dies (underpopulation)
+					(Cell::Alive, x) if x < 2 => Cell::Dead,
+					// Rule 2: Cell::Alive AND (2 <= live_neighbors <= 3) -> alive
+					(Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+					// Rule 3: Cell::Alive AND (live_neighbors > 3) -> dies (overpopulation)
+					(Cell::Alive, x) if x > 3 => Cell::Dead,
+					// Rule 4: Cell::Dead AND (live_neighbors == 3) -> alive (reproduction)
+					(Cell::Dead, 3) => Cell::Alive,
+					// All other cells remain in the same state
+					(otherwise, _) => otherwise,
+				};
+
+				next[idx] = next_cell;
+			}
+		}
+
+		self.cells = next;
+	}
 }
